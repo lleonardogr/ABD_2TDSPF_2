e212ca3fe43703a85ca5428d9ecd68f50c4194e2
lleonardogr <lleonardogr@gmail.com>
2024-08-23 18:08:11 -0300

    chore: Refactor CardEndpoint to use endpoint grouping

--- CardGameManagement.Api/Configuration/Routes/CardEndpoint.cs
+++ CardGameManagement.Api/Configuration/Routes/CardEndpoint.cs
@@ -11,65 +11,82 @@ public static class CardEndpoint
     {
         var cards = new List<Card>()
         {
-            new(){Id = 1, Name = "Card 1"},
-            new(){Id = 2, Name = "Card 2"}
+            new() { Id = 1, Name = "Card 1" },
+            new() { Id = 2, Name = "Card 2" }
         };
 
+        // Define o grupo de endpoints para "card"
+        var cardGroup = app.MapGroup("/cards");
+
         #region Queries
-        
-        app.MapGet("/cards", () => cards).WithName("Buscar cards").WithTags("card")
-            .WithOpenApi(operation => new (operation)
+
+        cardGroup.MapGet("/", () => cards)
+            .WithName("Buscar cards")
+            .WithOpenApi(operation => new(operation)
             {
                 OperationId = "GetCards",
                 Summary = "Retorna todos os cards",
                 Description = "Retorna todos os cards",
                 Deprecated = true
-            }).Produces<List<Card>>().ExcludeFromDescription();
+            })
+            .Produces<List<Card>>()
+            .ExcludeFromDescription();
 
-        app.MapGet("/cards/search", (int page, int pagesize) =>
+        cardGroup.MapGet("/search", (int page, int pagesize) =>
         {
             var skip = (page - 1) * pagesize;
             var results = cards.Skip(skip).Take(pagesize).ToList();
-            return new CardListModel() { Page = page, PageSize = pagesize, Total = results.Count, 
-                Cards = results.Select(c => 
-                    new CardSummaryModel(){Id = c.Id, Name = c.Name, Description = c.Description, ImageUrl = c.ImageUrl}).ToList()};
+            return new CardListModel()
+            {
+                Page = page,
+                PageSize = pagesize,
+                Total = results.Count,
+                Cards = results.Select(c => new CardSummaryModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Description = c.Description,
+                    ImageUrl = c.ImageUrl
+                }).ToList()
+            };
         });
 
-        app.MapGet("/cards/{id:int}", (int id) =>
-            {
-                return cards.FirstOrDefault(c => c.Id == id) is { } card ? Results.Ok(card) : Results.NotFound();
-            }).WithTags("card")
+        cardGroup.MapGet("/{id:int}",
+                (int id) =>
+                {
+                    return cards.FirstOrDefault(c => c.Id == id) is { } card ? Results.Ok(card) : Results.NotFound();
+                })
             .Produces<Card>()
             .Produces(StatusCodes.Status404NotFound);
-        
+
         #endregion
 
-        #region  Commands
-      
-        app.MapPost("/cards", (CardAddOrUpdateModel model) =>
-            {
-                cards.Add(model.MapToCard());
-            })
-            .Accepts<Card>("application/json")
-            .WithTags("card").WithName(" Add card").WithOpenApi();
+        #region Commands
 
-        app.MapPut("/cards/{id:int}", (int id, Card card) =>
-        {
-            var cardToUpdate = cards.FirstOrDefault(c => c.Id == id);
-    
-            if (cardToUpdate == null) 
-                return;
-    
-            cardToUpdate = card;
-            cardToUpdate.Id = id;
-        }).WithTags("card").WithName("Atualizar card").WithOpenApi(generatedOperation =>
-        {
-            var parameter = generatedOperation.Parameters[0];
-            parameter.Description = "Id do card a ser atualizado";
-            parameter.Required = true;
-            return generatedOperation;
-        });
-        
+        cardGroup.MapPost("/", (CardAddOrUpdateModel model) => { cards.Add(model.MapToCard()); })
+            .Accepts<Card>("application/json")
+            .WithName("Add card")
+            .WithOpenApi();
+
+        cardGroup.MapPut("/{id:int}", (int id, Card card) =>
+            {
+                var cardToUpdate = cards.FirstOrDefault(c => c.Id == id);
+
+                if (cardToUpdate == null)
+                    return;
+
+                cardToUpdate = card;
+                cardToUpdate.Id = id;
+            })
+            .WithName("Atualizar card")
+            .WithOpenApi(generatedOperation =>
+            {
+                var parameter = generatedOperation.Parameters[0];
+                parameter.Description = "Id do card a ser atualizado";
+                parameter.Required = true;
+                return generatedOperation;
+            });
+
         #endregion
     }
 }
